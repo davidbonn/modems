6fe27f9eabdae2fbb241ab1c0cd2653b9a2a8cb0
davidbonn <davidbonn@mac.com>
2021-08-19 20:03:46 -0700

    option to do fixes until we hit a threshold hdop will set redis values at best effort until we hit the threshold

--- gps.py
+++ gps.py
@@ -7,20 +7,69 @@
 # Copyright (C) 2021 Deepseek Labs, Inc.
 
 # TODO:  have it come up in parallel to shorten bootup time
+# TODO:  run continuously as you get more accurate results
 
 import argparse
+import time
 
 from common.rediswrapper import RedisWrapper
 from telit import Telit, check_for_telit
+
+R = RedisWrapper()
+
+
+def set_coordinates(pos, verbose):
+    global R
+
+    if pos is not None:
+        R["LATITUDE"] = pos["latitude"]
+        R["LONGITUDE"] = pos["longitude"]
+
+        if verbose:
+            print(f"[gps] GPS fix:  {pos['latitude']:.3f}, {pos['longitude']:.3f}")
+
+
+def get_gps_fix(t, verbose, retries):
+    pos = t.get_position(2.0, total=retries)
+
+    if pos is None:
+        if verbose:
+            print(f"[gps] Unable to get GPS fix")
+
+    set_coordinates(pos, verbose)
+
+
+def get_continuous_gps_fix(t, verbose, until):
+    last_pos = None
+    counter = 1
+
+    while True:
+        pos = t.get_position(until, total=12)
+
+        if pos is None:
+            if verbose:
+                print(f"[gps] No GPS fix #{counter}")
+                counter += 1
+
+            time.sleep(10)
+        elif last_pos is None or pos["hdop"] < last_pos["hdop"]:
+            last_pos = pos
+            set_coordinates(last_pos, verbose)
+
+            if pos["hdop"] <= until:
+                if verbose:
+                    print(f"[gps] Got GPS fix ({pos['hdop']:.3f})")
+
+                return
 
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("--verbose", default=False, action='store_true')
     ap.add_argument("--retries", default=40, type=int)
+    ap.add_argument("--until", default=None, type=float, required=False)
 
     args = ap.parse_args()
-    R = RedisWrapper()
 
     if not check_for_telit():
         print(f"[gps] No telit card, exiting")
@@ -32,18 +81,10 @@ def main():
         if t.get_gpsp_status() == 0:
             _ = t.send_gpsp_on()
 
-        pos = t.get_position(1.5, total=args.retries)
-
-        if pos is None:
-            if args.verbose:
-                print(f"[gps] Unable to get GPS fix")
-
-        if pos is not None:
-            R["LATITUDE"] = pos["latitude"]
-            R["LONGITUDE"] = pos["longitude"]
-
-            if args.verbose:
-                print(f"[gps] GPS fix:  {pos['latitude']:.3f}, {pos['longitude']:.3f}")
+        if args.until is None:
+            get_gps_fix(t, args.verbose, args.retries)
+        else:
+            get_continuous_gps_fix(t, args.verbose, args.until)
 
         _ = t.send_gpsp_off()
 
