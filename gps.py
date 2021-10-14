42a72a927fe396d68a3e78cf8ef6bc01f52266b7
davidbonn <davidbonn@mac.com>
2021-10-14 14:48:25 -0700

    stash in /tmp/deepseek/location.json read initial coordinates from /boot/deepseek/location.json

--- gps.py
+++ gps.py
@@ -8,20 +8,39 @@
 
 # TODO:  have it come up in parallel to shorten bootup time
 # TODO:  run continuously as you get more accurate results
+# TODO:  read from /boot/deepseek/location.json for initial values
+# TODO:  write to /tmp/deepseek/location.json as you refine the location
 
 import argparse
 import time
+import os
+import math
+import json
 
 from common.rediswrapper import RedisWrapper
 from telit import Telit, check_for_telit
 
 R = RedisWrapper()
+Location_src = "/boot/deepseek/location.json"
+Location_tmp = "/tmp/deepseek/location.json"
+
+
+def coord_within_tolerance(coord1, coord2):
+    """see if coord1 and coord2 are equal within tolerances (four decimal places)"""
+    r_lat = math.isclose(coord1["latitude"], coord2["latitude"], rel_tol=1e-7)
+    r_lon = math.isclose(coord1["longitude"], coord2["longitude"], rel_tol=1e-7)
+
+    return r_lat and r_lon
 
 
 def set_coordinates(pos, verbose):
+    """note coordinates found to REDIS"""
     global R
 
     if pos is None:
+        return
+
+    if R["HDOP"] is not None and pos["hdop"] <= R["HDOP"]:
         return
 
     R["LATITUDE"] = pos["latitude"]
@@ -33,7 +52,56 @@ def set_coordinates(pos, verbose):
         print(f"[gps] GPS fix:  {pos['latitude']:.4f}, {pos['longitude']:.4f}")
 
 
+def update_coordinates(pos, verbose):
+    """
+    update /tmp/deepseek/location.json if necessary
+
+    "necessary" is when the HDOP has improved and the coordinates have changed
+    """
+
+    if pos is None:
+        return
+
+    value = dict()
+    for k in ["latitude", "longitude", "altitude", "hdop"]:
+        value[k] = pos[k]
+
+    value["latitude"] = round(value["latitude"], 4)
+    value["longitude"] = round(value["longitude"], 4)
+
+    if os.path.exists(Location_tmp):
+        with open(Location_tmp, "r") as f:
+            current = json.load(f)
+
+        if value["hdop"] >= current["hdop"] or coord_within_tolerance(current, value):
+            return
+
+    if verbose:
+        print(
+            f"[gps] updating gps coordinates to {Location_tmp} ({value['latitude']:.4f},{value['longitude']:.4f})")
+
+    with open(Location_tmp, "w") as f:
+        json.dump(value, f)
+
+
+def initialize_coordinates(verbose):
+    """read coordinates from /boot/deepseek and note them in REDIS and /tmp/deepseek"""
+    if not os.path.exists(Location_src):
+        return
+
+    with open(Location_src, "r") as f:
+        loc = json.load(f)
+
+    # sleazy hack
+    # this lets us update with better coordinates or if we have changed the location of the camera
+    loc["hdop"] = 9999.00
+
+    set_coordinates(loc, verbose)
+    update_coordinates(loc, verbose)
+
+
 def get_gps_fix(t, verbose, retries):
+    """get one gps fix"""
     pos = t.get_position(2.0, total=retries)
 
     if pos is None:
@@ -41,9 +109,11 @@ def get_gps_fix(t, verbose, retries):
             print(f"[gps] Unable to get GPS fix")
 
     set_coordinates(pos, verbose)
+    update_coordinates(pos, verbose)
 
 
 def get_continuous_gps_fix(t, verbose, until):
+    """get a gps fix until the hdop value is below "until" """
     last_pos = None
     counter = 1
 
@@ -59,6 +129,7 @@ def get_continuous_gps_fix(t, verbose, until):
         elif last_pos is None or pos["hdop"] < last_pos["hdop"]:
             last_pos = pos
             set_coordinates(last_pos, verbose)
+            update_coordinates(last_pos, verbose)
 
             if pos["hdop"] <= until:
                 if verbose:
